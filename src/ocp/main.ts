17dc90a14f906910a70cea2dbe92ec1e284a5081
Vyctor <dev.vyctor@gmail.com>
2021-06-04 19:44:55 -0300

    open closed principle #1

---
+++ src/ocp/main.ts
@@ -0,0 +1,24 @@
+/**
+ * Open/closed principle
+ * Entidades devem estar abertas para extensão, mas fechadas para modificação.
+ */
+import { Order } from './classes/order';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { NoDiscount } from './classes/discount';
+
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency(messaging);
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 50.0));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lápis', 2.1));
+
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
